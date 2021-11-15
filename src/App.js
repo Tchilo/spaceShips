056130a49dbd31052ea248a6772388afd73b5fec
Tchilo <tchiloross@gmail.com>
2021-11-15 14:55:49 +0200

    Add app code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
-const App = () => (<div />);
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
+import Rockets from './components/Rockets';
+import Navbar from './components/Navbar';
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/my-profile" element={<Profile />} />
+    </Routes>
+
+  </Router>
+);
 
 export default App;
