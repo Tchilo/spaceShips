4d9108ec1f719c8d49cf012432af2fb617625609
Tchilo <tchiloross@gmail.com>
2021-11-16 15:54:46 +0200

    Add reserve and cancel reservation button

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getRockets } from '../redux/rockets/rockets';
 
 function Rockets() {
   const rocketStore = useSelector((state) => state.rocketReducer);
-  // const [showResrved, setshowReserved] = useState(true);
+  const [showResrved, setshowReserved] = useState(true);
   const dispatch = useDispatch();
   useEffect(() => {
     if (!rocketStore.length) {
@@ -17,16 +17,18 @@ function Rockets() {
         id, images, names, description,
       } = rocket;
       return (
+
         <ul key={id}>
           <li key={id}>
             <img src={images[0]} alt={names} />
             {rocket.names}
             {' '}
             <p>{description}</p>
-            <button type="button">Rocket Reserve</button>
-            <button type="button">Cancel Reservation </button>
+            {(showResrved && <button type="button" onClick={() => setshowReserved(false)}>Rocket Reserve</button>)}
+            {(!showResrved && <button type="button" onClick={() => setshowReserved(true)}>Cancel Reservation </button>)}
           </li>
         </ul>
+
       );
     })
   );
