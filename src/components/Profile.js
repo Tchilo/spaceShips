76e19507c5c8046671e5b745097f3c6008ebb5f9
yahya <crazy20046@gmail.com>
2021-11-17 10:52:21 +0200

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,38 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missions';
 
 function Profile() {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+  useEffect(() => {
+    if (!missions.length) {
+      dispatch(fetchMissions);
+    }
+  }, []);
+  const reservedMissions = missions.filter((mission) => mission.reserved === true && mission);
   return (
-    <div>
-      Profile
+    <div >
+      {
+        reservedMissions.length
+          ? (
+            <>
+              <h2>My Missions</h2>
+              <ul>
+                {reservedMissions.map((mission) => (
+                  <li key={mission.id}>
+                    {mission.name}
+                  </li>
+                ))}
+              </ul>
+
+            </>
+          )
+          : (
+            <h2>There are no missions reserved</h2>
+          )
+      }
     </div>
   );
 }
