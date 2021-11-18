2b9a98060b5dcbd3237c4a0c1e378d889914fd3f
yahya <crazy20046@gmail.com>
2021-11-18 11:30:58 +0200

    Fixing conflict

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -8,87 +8,87 @@ const FETCHING_ROCKETS_FAILED = 'FETCHING_ROCKETS_FAILED';
 const CANCEL_RESERVATION = 'CANCEL_RESERVATION';
 
 const initialState = {
-  rockets: [],
+    rockets: [],
 };
 
 const loadRockets = (rockets) => ({
-  type: GET_ROCKETS,
-  payload: rockets,
+    type: GET_ROCKETS,
+    payload: rockets,
 });
 
 export const reserve = (id) => ({
-  type: RESERVE_ROCKET,
-  id,
+    type: RESERVE_ROCKET,
+    id,
 });
 
 export const cancelReservation = (id) => ({
-  type: CANCEL_RESERVATION,
-  id,
+    type: CANCEL_RESERVATION,
+    id,
 });
 
 const fetchingDataFailed = (err) => ({
-  type: FETCHING_ROCKETS_FAILED,
-  payload: err,
+    type: FETCHING_ROCKETS_FAILED,
+    payload: err,
 });
 
 export const fetchRockets = () => async (dispatch) => {
-  try {
-    const response = await fetch(url);
-    const rockets = await response.json();
-    console.log(rockets);
-    dispatch(
-      loadRockets(
-        rockets.map((rocket) => {
-          const {
-            id, rocket_name: names, flickr_images: images, description,
-          } = rocket;
-          return {
-            id, names, images, description,
-          };
-        }),
-      ),
-    );
-  } catch (err) {
-    err.description = 'An error occurred, please try again later';
-    dispatch(fetchingDataFailed(err.description));
-  }
+    try {
+        const response = await fetch(url);
+        const rockets = await response.json();
+        console.log(rockets);
+        dispatch(
+            loadRockets(
+                rockets.map((rocket) => {
+                    const {
+                        id, rocket_name: names, flickr_images: images, description,
+                    } = rocket;
+                    return {
+                        id, names, images, description,
+                    };
+                }),
+            ),
+        );
+    } catch (err) {
+        err.description = 'An error occurred, please try again later';
+        dispatch(fetchingDataFailed(err.description));
+    }
 };
 
 const rocketReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case GET_ROCKETS:
-      return {
-        ...state,
-        rockets: action.payload,
-      };
+    switch (action.type) {
+        case GET_ROCKETS:
+            return {
+                ...state,
+                rockets: action.payload,
+            };
 
-    case FETCHING_ROCKETS_FAILED:
-      return {
-        ...state,
-        loading: false,
-        error: action.payload,
-      };
-    case RESERVE_ROCKET:
-      const newState = state.rockets.map((rocket) => {
-        if (rocket.id !== action.id) { return rocket; }
-        return { ...rocket, reserved: true };
-      });
-      return {
-        ...state,
-        rockets: newState,
-      };
-    case CANCEL_RESERVATION:
-      const nextState = state.rockets.map((rocket) => {
-        if (rocket.id !== action.id) { return rocket; }
-        return { ...rocket, reserved: false };
-      });
-      return {
-        ...state,
-        rockets: nextState,
-      };
-    default:
-      return state;
-  }
+        case FETCHING_ROCKETS_FAILED:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+        case RESERVE_ROCKET:
+            const newState = state.rockets.map((rocket) => {
+                if (rocket.id !== action.id) { return rocket; }
+                return { ...rocket, reserved: true };
+            });
+            return {
+                ...state,
+                rockets: newState,
+            };
+        case CANCEL_RESERVATION:
+            const nextState = state.rockets.map((rocket) => {
+                if (rocket.id !== action.id) { return rocket; }
+                return { ...rocket, reserved: false };
+            });
+            return {
+                ...state,
+                rockets: nextState,
+            };
+        default:
+            return state;
+    }
 };
 
 export default rocketReducer;
